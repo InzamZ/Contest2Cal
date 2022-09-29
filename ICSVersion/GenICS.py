ac932018b38fcc7d822e59b2256faa60c288d2af
InzamZ <me@inzamz.top>
2022-09-29 17:03:52 +0800

    :sparkles: 日程更新机制的完善，保留过期日程，更新未来日程

--- ICSVersion/GenICS.py
+++ ICSVersion/GenICS.py
@@ -12,26 +12,42 @@ def GetContestList(url: str = "https://algcontest.rainng.com/") -> list:
     return []
 
 
-def GenerateICS(contestList: list, path: str = "./sample.ics", oldCal: Calendar = None) -> str:
+def GenerateICS(contestList: list, path: str = "./ICSVersion/sample.ics", oldCal: Calendar = None) -> str:
     cal = oldCal
-    if cal == None:
+    uid2event = {}
+    if cal:
+        for component in cal.walk():
+            if (component.name == "VEVENT"):
+                uid2event[component.get('uid')] = component
+    else:
         cal = Calendar()
-    cal.add('VERSION', '2.0')
-    cal.add('prodid', '各大OJ比赛日历ICS')
-    cal.add('X-WR-CALNAME', '各大OJ比赛日历ICS')
-    cal.add('X-APPLE-CALENDAR-COLOR', '#540EB9')
-    cal.add('X-WR-TIMEZONE', 'Asia/Shanghai')
+        cal.add('VERSION', '2.0')
+        cal.add('prodid', '各大OJ比赛日历ICS')
+        cal.add('X-WR-CALNAME', '各大OJ比赛日历ICS')
+        cal.add('X-APPLE-CALENDAR-COLOR', '#540EB9')
+        cal.add('X-WR-TIMEZONE', 'Asia/Shanghai')
+
     for contest in contestList:
+        curUid = contest['name'] + str(contest['startTimeStamp'])
         event = Event()
-        event.add('uid', contest['name'] + str(contest['startTimeStamp']))
-        event.add('summary', contest['name'])
-        dtstart = datetime.fromtimestamp(contest['startTimeStamp'])
-        dtend = datetime.fromtimestamp(contest['endTimeStamp'])
-        dstamp = datetime.utcnow()
-        event.add('dtstart', dtstart)
-        event.add('dtend', dtend)
-        event.add('dtstamp', dstamp)
-        event.add('description', contest['link'] + " \n数据来源http://algcontest.rainng.com/", encode=False)
+        if curUid in uid2event.keys():
+            event = uid2event[curUid]
+            event['uid'] = curUid
+            event['summary'] = contest['name']
+            event['dtstart'] = datetime.fromtimestamp(
+                contest['startTimeStamp'])
+            event['dtend'] = datetime.fromtimestamp(contest['endTimeStamp'])
+            event['dtstamp'] = datetime.utcnow()
+            event['dtstamp'] = contest['link'] + " \n数据来源http://algcontest.rainng.com/"
+        else:
+            event.add('uid', curUid)
+            event.add('summary', contest['name'])
+            event.add('dtstart', datetime.fromtimestamp(
+                contest['startTimeStamp']))
+            event.add('dtend', datetime.fromtimestamp(contest['endTimeStamp']))
+            event.add('dtstamp', datetime.utcnow())
+            event.add(
+                'description', contest['link'] + " \n数据来源http://algcontest.rainng.com/", encode=False)
         cal.add_component(event)
         # print(event.to_ical())
 
@@ -41,5 +57,12 @@ def GenerateICS(contestList: list, path: str = "./sample.ics", oldCal: Calendar 
 
 
 if __name__ == "__main__":
-    with open("../SampleData/AlgContest.json") as f:
-        GenerateICS(json.loads(f.read()))
+    data = open("./SampleData/AlgContest.json")
+    cal = None
+    try:
+        ics = open("./ICSVersion/sample.ics")
+        cal = Calendar.from_ical(ics.read())
+    except:
+        cal = None
+        
+    GenerateICS(json.loads(data.read()), oldCal=cal)
